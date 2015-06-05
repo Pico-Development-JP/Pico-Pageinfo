6af8495b6ebcc5b88bf857f31e8917bc06dee587
高見知英 <chie@onpu-tamago.net>
2015-06-05 16:48:19 +0900

    今後を見据えてファイルパスの変更

---
+++ pageinfo.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Pico PageInfo Plugin
+ *
+ * @author TakamiChie
+ * @link http://onpu-tamago.net/
+ * @license http://opensource.org/licenses/MIT
+ * @version 1.0
+ */
+class PageInfo{
+
+  // copied from pico source, $headers as array gives ability to add additional metadata, e.g. header image
+  private function read_file_meta($file_url, $content) {
+    $headers = array('attachurl' => 'URL');
+
+    foreach ($headers as $field => $regex) {
+      if (preg_match('/^[ \t\/*#@]*' . preg_quote($regex, '/') . ':(.*)$/mi', $content, $match) && $match[1]){
+        $headers[ $field ] = trim(preg_replace("/\s*(?:\*\/|\?>).*/", '', $match[1]));
+      } else {
+        $headers[ $field ] = '';
+      }
+    }
+
+    $headers['p_path'] = $file_url;
+    $p = explode("/", $file_url);
+    array_shift($p); // 一つ目の要素は必ずコンテンツフォルダのため
+    $headers['p_name'] = array_pop($p); // 最後の項目はファイル名のため
+    $headers['p_pathes'] = $p;
+    $headers['p_depth'] = substr_count($file_url, "/");
+
+		return $headers;
+  }
+  
+  public function config_loaded(&$settings) {
+    $this->base_url = $settings['base_url'];
+  }
+
+  public function get_pages(&$pages, &$current_page, &$prev_page, &$next_page) {
+
+    $ifmerge = function (&$page, &$looking_page, &$file_meta) {
+      if($page['url'] == $looking_page['url']){
+        $page = array_merge($page, $file_meta);
+      }
+    };
+    
+    $new_pages = array();
+    foreach ($pages as $page) {
+      $file_url = substr($page["url"], strlen($this->base_url));
+      if($file_url[strlen($file_url) - 1] == "/") $file_url .= 'index';
+      $file_name = CONTENT_DIR . $file_url . ".md";
+      
+      // get metadata from page
+      if (file_exists($file_name)) {
+        $file_content = file_get_contents($file_name);
+        $file_meta = $this->read_file_meta($file_url, $file_content);
+        $page = array_merge($page, $file_meta);
+        array_push($new_pages, $page);
+        $ifmerge($current_page, $page, $file_meta);
+        $ifmerge($next_page, $page, $file_meta);
+        $ifmerge($prev_page, $page, $file_meta);
+      }
+    }
+    $pages = $new_pages;
+  }
+}
+
+?>
